4d9e9584fc31b97e3f40222a2bf159a935959b52
Yuxin-Ji <ji.yuxin@hotmail.com>
2020-09-24 23:37:26 +0800

    Added script exercise

---
+++ exercise1-jessica.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script takes as input one argument, a directory, and outputs the number of different permission types in this directory.
+ls -l | cut -d ' ' -f 1 | tail --lines=+1 | uniq | wc -l
